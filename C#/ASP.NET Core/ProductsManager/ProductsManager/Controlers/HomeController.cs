082f9de0cd614da0b1643ffdd6308b75511ea10e
ZelvaMan <zelvaman16@gmail.com>
2019-07-01 19:20:08 +0200

    ProductManager= fully working

---
+++ C#/ASP.NET Core/ProductsManager/ProductsManager/Controlers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductsManager.Models;
+
+namespace ProductsManager.Controlers
+{
+	public class HomeController : Controller
+	{
+		private ProductCollections Database;
+		public ActionResult Index()
+		{
+			return RedirectToAction("ShowProducts");
+		}
+
+		[HttpGet("Product")]
+		public ActionResult ShowProducts()
+		{
+			ViewBag.data = Database.Products;
+			return View();
+		}
+
+		[HttpGet("Product/{ID}")]
+		public ActionResult ShowBuyProduct(int ID)
+		{
+			Product product = Database.Products.Find((p => p.ID == ID));
+			if (product == null)
+			{
+				return Content("Product not found");
+			}
+
+			ViewBag.Product = product;
+			return View();
+		}
+
+		//[HttpPost("Product/buy/{ID}")]
+		[HttpPost("Product/{ID}")]
+		public ActionResult BuyProducts(int ID, int quantity)
+		{
+			Database.Buy(ID,quantity);
+			return RedirectToAction("ShowProducts");
+
+		}
+
+		[HttpGet("Order")]
+		public ActionResult ShowOrders()
+		{
+			ViewBag.Orders = Database.Orders;
+			return View();
+		}
+		public HomeController(ProductCollections productCollections)
+		{
+			Database = productCollections;
+		}
+	}
+}
---
+++ C#/ASP.NET Core/ProductsManager/ProductsManager/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsManager.Models
+{
+	public class Product
+	{
+		public int ID { get;  set; }
+		public string Name { get;  set; }
+		public int Price { get; set; }
+		public int InStock { get; set; }
+	}
+
+	public struct Order
+	{
+		public int ID { get; set; }
+		public int Quantity { get; set; }
+		public int TotalPrice { get; set; }
+
+	}
+}
