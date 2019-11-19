93ebd10425272ad9eca900ffb9dc3470cc650d75
ZelvaMan <honza.rada1@seznam.cz>
2019-11-19 16:58:12 +0100

    WORKING

--- svelte/my-svelte-project/src/overview.svelte
+++ svelte/my-svelte-project/src/overview.svelte
@@ -1,18 +1,44 @@
  <script>
+  import SortForm from "./SortForm.svelte";
+  import { Sort } from "../helpers/filter.js";
+ <script>
  import SortForm from "./SortForm.svelte";
   import people from "../data/people.js";
   import {Sort} from "../helpers/filter.js";
   import Card from "./Card.svelte";
+
   
   export let params = {};
+  let parameters = null;
+  const BaseAPiUrl = "https://localhost:44360/api/Users/GetUsers";
+  let filteredPeople;
   
   console.log(people);
 
+  $: filteredPeople = LoadFromApi(parameters);
   let parameters;
   
   $: filteredPeople = Sort(parameters, people);
 
+  async function LoadFromApi(parms) {
+    let url = null;
+    console.log("fetch URL:" + url);
+    const response = await fetch(BaseAPiUrl);
+    filteredPeople = await response.json();
+    console.log(filteredPeople);
+  }
 
+  function ChangeParms(event) {
+    if (event) parameters = event.detail;
+  }
+
+  function LogProperties() {
+    console.log("filteredPeople:");
+    console.log(filteredPeople);
+
+    console.log("----------------------------------------");
+    console.log("params:");
+    console.log(parameters);
   function ChangeParms(event){
     console.log("event handled");
     console.log("event details:" + event.detail);
@@ -21,7 +47,7 @@
   }
 </script>
 
-<style>
+@ -36,31 +25,21 @@
   .margin {
     margin-bottom: 200px;
   }
@@ -29,12 +55,30 @@
 </style>
 
 <div class="section">
+<button on:click = {LogProperties}>LOG</button>
+  <div class="container">
+    <SortForm
+      on:parmChanged={ChangeParms}
+      filterName={params.filtername}
+      filterValue={params.filtervalue} />
   <div class="container">      
     <SortForm on:parmChanged={ChangeParms} filterName={params.filtername} filterValue={params.filtervalue} />
   </div>
   <br class="margin" />
   <div class="container">
     <div class="columns is-centered is-multiline" style="padding: 2rem">
+      {#if filteredPeople == null || filteredPeople == undefined}
+        <h1>LOADING FROM DATABASE</h1>
+      {:else}
+      
+        else
+        {#each filteredPeople as person}
+        
+          <div class="column is-2">
+            <Card bind:userInfo={person} />
+          </div>
+        {/each}
+      {/if}
       {#each filteredPeople as person, i}
         <div class="column is-2">
           <Card bind:userInfo={person} />
